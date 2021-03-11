df3888862bcc108a4fda31ecc09e898fef96cf88
EduardAnt <80110441+EduardAnt@users.noreply.github.com>
2021-03-11 16:24:29 -0500

    I added another line to your code

--- hello2.java
+++ hello2.java
@@ -1,5 +1,6 @@
 public class hello2{
   public static void main(String[] args){
-    System,out.println("Hello, World! Again!")
+    System.out.println("Hello, World! Again!");
+    System.out.println("Hello, Lab3 is done!");
   }
 }
